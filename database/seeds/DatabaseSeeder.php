4603e4d69efb851845021e2aefa927a18aff9c9d
Daniel Montañez <gibasedatosydesarrollo@hlips.com.co>
2022-02-21 09:53:17 -0500

    Ajuste de TypeofAttention seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -84,7 +84,7 @@ class DatabaseSeeder extends Seeder
                 $this->call(ObjetionCodeResponseSeeder::class);
                 $this->call(ObjetionTypeSeeder::class);
                 $this->call(RepeatedInitialSeeder::class);
-                $this->call(TypeOfAttention::class);
+                $this->call(TypeOfAttentionSeeder::class);
                 $this->call(FrequencySeeder::class);
                 //$this->call(CriterionSeeder::class);
                 //$this->call(CriterionActivityGoalSeeder::class);
