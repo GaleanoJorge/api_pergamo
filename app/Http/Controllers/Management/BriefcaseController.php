95ba112aaf420f9b1f293b5b89e3589c0f64b5b7
desarrollador <gidesarrollador@hlips.com.co>
2022-03-30 11:09:37 -0500

    corrección búsqueda de portafolio

--- app/Http/Controllers/Management/BriefcaseController.php
+++ app/Http/Controllers/Management/BriefcaseController.php
@@ -52,7 +52,7 @@ class BriefcaseController extends Controller
      * @param  int  $contractId
      * @return JsonResponse
      */
-    public function getByContract(Request $request, int $contractId): JsonResponse
+    /*public function getByContract(Request $request, int $contractId): JsonResponse
     {
         $Briefcase = Briefcase::where('contract_id', $contractId)
             ->orderBy('name','asc')->get()->toArray();
@@ -62,36 +62,36 @@ class BriefcaseController extends Controller
             'message' => 'Portafolios por contrato obtenido exitosamente',
             'data' => ['briefcase' => $Briefcase]
         ]);
-    }
+    }*/
 
-    //  /**
-    //  * Get procedure by manual.
-    //  *
-    //  * @param  int  $contractId
-    //  * @return JsonResponse
-    //  */
-    // public function getByContract(Request $request, int $contractId): JsonResponse
-    // {
-    //     $Briefcase = Briefcase::where('contract_id', $contractId)->with('type_briefcase','coverage','modality','status');
-    //     if ($request->search) {
-    //         $Briefcase->where('name', 'like', '%' . $request->search . '%')
-    //         ->Orwhere('id', 'like', '%' . $request->search . '%');
-    //     }
-    //     if ($request->query("pagination", true) === "false") {
-    //         $Briefcase = $Briefcase->get()->toArray();
-    //     } else {
-    //         $page = $request->query("current_page", 1);
-    //         $per_page = $request->query("per_page", 10);
+      /**
+      * Get procedure by manual.
+      *
+      * @param  int  $contractId
+      * @return JsonResponse
+      */
+     public function getByContract(Request $request, int $contractId): JsonResponse
+     {
+         $Briefcase = Briefcase::where('contract_id', $contractId)->with('type_briefcase','coverage','modality','status');
+         if ($request->search) {
+             $Briefcase->where('name', 'like', '%' . $request->search . '%')
+             ->Orwhere('id', 'like', '%' . $request->search . '%');
+         }
+         if ($request->query("pagination", true) === "false") {
+             $Briefcase = $Briefcase->get()->toArray();
+         } else {
+             $page = $request->query("current_page", 1);
+             $per_page = $request->query("per_page", 10);
 
-    //         $Briefcase = $Briefcase->paginate($per_page, '*', 'page', $page);
-    //     }
+             $Briefcase = $Briefcase->paginate($per_page, '*', 'page', $page);
+         }
 
-    //     return response()->json([
-    //         'status' => true,
-    //         'message' => 'Portafolio por contrato obtenido exitosamente',
-    //         'data' => ['briefcase' => $Briefcase]
-    //     ]);
-    // }
+         return response()->json([
+             'status' => true,
+             'message' => 'Portafolio por contrato obtenido exitosamente',
+             'data' => ['briefcase' => $Briefcase]
+         ]);
+     }
 
 
     public function store(BriefcaseRequest $request): JsonResponse
