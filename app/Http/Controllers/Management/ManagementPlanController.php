b4addc8452cca66f72d059001c709ce0abe7a677
Jorge Galeano <gidesarrolladorapp1@hlips.com.co>
2022-04-10 17:01:54 -0500

    manage_plan descuenta capacidad instalada del profesional

--- app/Http/Controllers/Management/ManagementPlanController.php
+++ app/Http/Controllers/Management/ManagementPlanController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Management;
 use App\Models\ManagementPlan;
 use App\Models\AssignedManagementPlan;
 use App\Models\Frequency;
-use App\Models\Location;
 use App\Models\Bed;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ManagementPlanRequest;
+use App\Models\BaseLocationCapacity;
+use App\Models\LocationCapacity;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -96,37 +97,89 @@ class ManagementPlanController extends Controller
         $ManagementPlan->procedure_id = $request->procedure_id;
         $ManagementPlan->save();
 
+        $error = false;
+        // cambiar por calculo de días entre fecha inicial y fehca final
+        // <------------------------------------------------------------------------------------->
         $frequency = Frequency::where('id', $request->frequency_id)->get()->toArray();
         foreach ($frequency as $key => $row) {
-            $diferencei = $row['days'] / $request->quantity;
+            $diferencei = ceil($row['days'] / $request->quantity);
         }
-        $now = Carbon::now();
-        $finish = Carbon::now()->addDays($diferencei);
-        $diference = 7;
+        // <------------------------------------------------------------------------------------->
+        // $now = Carbon::now();
+        // $finish = Carbon::now()->addDays($diferencei);
+        // $diference = 7;
+        $firstDateMonth = Carbon::now()->startOfMonth();
+        $lastDateMonth = Carbon::now()->endOfMonth();
+        $startDate = Carbon::now()->startOfDay(); // cambiar la fecha actual por la fecha inicial
+        $diferenceDate = Carbon::now()->addDays($diferencei - 1)->endOfDay();
         for ($i = 0; $i < $request->quantity; $i++) {
 
             if ($i == 0) {
-                $start = $now;
-                $finish = $finish;
+                $start = $startDate->copy();
+                $finish = $diferenceDate->copy();
             } else {
-                $diference = $diference + $diferencei;
-                $start = $finish->addDays(1);
-                $finish = Carbon::now()->addDays($diference);
+                // $diference = $diference + $diferencei;
+                // $start = $finish->addDays(1);
+                // $finish = Carbon::now()->addDays($diference);
+                $start = $diferenceDate->addDays(1)->startOfDay()->copy();
+                $finish =  $diferenceDate->addDays($diferencei - 1)->endOfDay()->copy();
             }
+
+            $assigned = false;
+            while (!$assigned && !$error) {
+                if ($start->between($firstDateMonth, $lastDateMonth)) {
+                    $locattionCapacity = LocationCapacity::where('assistance_id', $request->assistance_id)
+                        ->where('locality_id', $request->locality_id)
+                        ->where('validation_date', '>=', $firstDateMonth)->where('validation_date', '<=', $lastDateMonth)->first();
+                    if ($locattionCapacity) {
+                        $locattionCapacity->PAD_patient_actual_capacity = $locattionCapacity->PAD_patient_actual_capacity - 1;
+                        $locattionCapacity->save();
+                        $assigned = true;
+                    } else {
+                        $baseLocationCapacity = BaseLocationCapacity::where('assistance_id', $request->assistance_id)
+                            ->where('locality_id', $request->locality_id)->first();
+                        if ($baseLocationCapacity) {
+                            $newLocationCapacity = new LocationCapacity;
+                            $newLocationCapacity->assistance_id = $request->assistance_id;
+                            $newLocationCapacity->locality_id = $request->locality_id;
+                            $newLocationCapacity->PAD_patient_quantity = $baseLocationCapacity->PAD_base_patient_quantity;
+                            $newLocationCapacity->PAD_patient_attended = 0;
+                            $newLocationCapacity->PAD_patient_actual_capacity = $baseLocationCapacity->PAD_base_patient_quantity - 1;
+                            $newLocationCapacity->validation_date = $start;
+                            $newLocationCapacity->save();
+                            $assigned = true;
+                        } else {
+                            $error = true;
+                        }
+                    }
+                } else {
+                    $firstDateMonth->addMonth();
+                    $lastDateMonth->subDays(15)->addMonth()->endOfMonth();
+                }
+            }
+
             $assignedManagement = new AssignedManagementPlan;
             $assignedManagement->start_date = $start;
             $assignedManagement->finish_date =  $finish;
-            $assignedManagement->user_id = $request->assigned_user_id;
+            $assignedManagement->user_id = !$error ? $request->assigned_user_id : null;
             $assignedManagement->management_plan_id = $ManagementPlan->id;
             $assignedManagement->save();
         }
 
-
-        return response()->json([
-            'status' => true,
-            'message' => 'Plan de manejo creado exitosamente',
-            'data' => ['management_plan' => $ManagementPlan->toArray()]
-        ]);
+        if (!$error) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Plan de manejo creado exitosamente',
+                'data' => ['management_plan' => $ManagementPlan->toArray()]
+            ]);
+        } else {
+            return response()->json([
+                'status' => true,
+                'message' => 'Plan de manejo creado exitosamente',
+                'message_error' => 'No se pudo asignar el plan de manejo de los meses posteriores ya que el médico no cuenta con capacidad instalada base en la localidad',
+                'data' => ['management_plan' => $ManagementPlan->toArray()]
+            ]);
+        }
     }
 
     /**
@@ -164,11 +217,11 @@ class ManagementPlanController extends Controller
         $ManagementPlan->assigned_user_id = $request->assigned_user_id;
         $ManagementPlan->save();
 
-        $validate=AssignedManagementPlan::where('management_plan_id',$id)->get()->toArray();
+        $validate = AssignedManagementPlan::where('management_plan_id', $id)->get()->toArray();
         foreach ($validate as $key => $value) {
-        $assignedManagement = AssignedManagementPlan::find($value['id']);
-        $assignedManagement->user_id = $request->assigned_user_id;
-        $assignedManagement->save();
+            $assignedManagement = AssignedManagementPlan::find($value['id']);
+            $assignedManagement->user_id = $request->assigned_user_id;
+            $assignedManagement->save();
         }
 
 
