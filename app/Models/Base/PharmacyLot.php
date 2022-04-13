1ff24d71da72334374c231ebee6b647501e991b5
Harold <gidesarrolladorapp2@hlips.com.co>
2022-04-13 16:12:28 -0500

    lote medicamentos

---
+++ app/Models/Base/PharmacyLot.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models\Base;
+
+use App\Models\Billing;
+use App\Models\PharmacyStock;
+use App\Models\Product;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class PharmacyLot
+ * 
+ * @property int $id 
+ * @property string $enter_amount
+ * @property string $unit_value
+ * @property string $lot
+ * @property date $expiration_date
+ * @property BigInteger $pharmacy_stock_id
+ * @property BigInteger $billing_id
+ * @property BigInteger $product_id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * 
+ *
+ * @package App\Models\Base
+ */
+class PharmacyLot extends Model
+{
+	protected $table = 'pharmacy_lot';
+
+
+	public function pharmacy_stock()
+	{
+		return $this->belongsTo(PharmacyStock::class, 'pharmacy_stock');
+	}
+
+
+	public function product()
+	{
+		return $this->belongsTo(Product::class);
+	}
+	
+	public function billing()
+	{
+		return $this->belongsTo(Billing::class);
+	}
+	
+}
