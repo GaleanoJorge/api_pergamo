f6fafb8132a1dddd046a1c7647528bc450bc7c6c
Ivan Sanchez <gidesarrollador@hlips.com.co>
2022-04-13 16:01:22 -0500

    Mod: amarrar plan de manejo a las auth creadas

--- app/Http/Controllers/Management/AuthorizationController.php
+++ app/Http/Controllers/Management/AuthorizationController.php
@@ -8,8 +8,12 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\AuthorizationRequest;
 use App\Models\AuthLog;
+use App\Models\Briefcase;
+use App\Models\ManagementPlan;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Auth;
+
+require('ManagementPlanController.php');
 
 
 class AuthorizationController extends Controller
@@ -56,9 +60,11 @@ class AuthorizationController extends Controller
     {
         $Authorization = Authorization::leftjoin('admissions', 'authorization.admissions_id', 'admissions.id')
             ->leftjoin('patients', 'admissions.patient_id', 'patients.id')
-            ->leftjoin('procedure','authorization.procedure_id', 'procedure.id')
+            ->leftjoin('briefcase', 'admissions.briefcase_id', 'briefcase.id')
+            ->leftjoin('procedure', 'authorization.procedure_id', 'procedure.id')
             ->select(
                 'authorization.*',
+                'briefcase.type_auth',
                 'patients.identification_type_id',
                 'patients.identification',
                 'patients.email',
@@ -74,15 +80,18 @@ class AuthorizationController extends Controller
                     ->with('admissions', 'identification_type', 'procedure', 'auth_status', 'residence_municipality', 'residence',);
             } else {
                 $Authorization->where('auth_status_id', $statusId)
-                    ->with('admissions', 'identification_type', 'procedure', 'auth_status', 'residence_municipality', 'residence',);
+                    ->with('admissions', 'briefcase', 'identification_type', 'procedure', 'auth_status', 'residence_municipality', 'residence',);
             }
         } else {
             if ($statusId == 0) {
-                $Authorization->where('auth_status_id', '!=', 3)
-                    ->where('auth_status_id', '!=', 4)
-                    ->with('admissions', 'identification_type', 'procedure', 'auth_status', 'residence_municipality', 'residence',);
+                $Authorization
+                    ->leftjoin('management_plan', 'management_plan.authorization_id', 'authorization.id')
+                    ->where('auth_status_id', '<', 3)
+                    ->with('admissions', 'management_plan', 'identification_type', 'procedure', 'auth_status', 'residence_municipality', 'residence',);
             } else {
-                $Authorization->where('auth_status_id', $statusId)
+                $Authorization
+                    ->leftjoin('management_plan', 'management_plan.authorization_id', 'authorization.id')
+                    ->where('auth_status_id', $statusId)
                     ->with('admissions', 'identification_type', 'procedure', 'auth_status', 'residence_municipality', 'residence',);
             }
         }
@@ -100,7 +109,7 @@ class AuthorizationController extends Controller
                     ->orWhere('lastname', 'like', '%' . $request->search . '%')
                     ->orWhere('middlelastname', 'like', '%' . $request->search . '%')
                     ->orWhere('auth_number', 'like', '%' . $request->search . '%');
-                });
+            });
         }
 
         if ($request->query("pagination", true) == "false") {
@@ -112,18 +121,32 @@ class AuthorizationController extends Controller
             $Authorization = $Authorization->paginate($per_page, '*', 'page', $page);
         }
 
-        return response()->json([
-            'status' => true,
-            'message' => 'Estados de glosas obtenidos exitosamente',
-            'data' => ['authorization' => $Authorization]
-        ]);
+        if ($type == 1) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Historico de autorizciones obtenido exitosamente',
+                'data' => ['authorization' => $Authorization]
+            ]);
+        } else {
+            return response()->json([
+                'status' => true,
+                'message' => 'Autorizaciones obtenidas exitosamente',
+                'data' => ['authorization' => $Authorization]
+            ]);
+        }
     }
 
     public function store(AuthorizationRequest $request): JsonResponse
     {
         $Authorization = new Authorization;
-        $Authorization->name = $request->name;
-        $Authorization->code = $request->code;
+        $Authorization->procedure_id =  $request->procedure_id;
+        $Authorization->admissions_id =  $request->id;
+        $validate = Briefcase::select('briefcase.*')->where('id',  $request->briefcase_id)->first();
+        if ($validate->auth_type == 1) {
+            $Authorization->auth_status_id =  2;
+        } else {
+            $Authorization->auth_status_id =  1;
+        }
 
         $Authorization->save();
 
@@ -161,16 +184,27 @@ class AuthorizationController extends Controller
     public function update(AuthorizationRequest $request, int $id): JsonResponse
     {
         $Authorization = Authorization::find($id);
-        if ($request->auth_number) {
+
+        if ($request->auth_number && $request->authorized_amount) {
+
+            $Authorization->auth_number = $request->auth_number;
+            $Authorization->authorized_amount = $request->authorized_amount;
+            $Authorization->auth_status_id = 3;
+        } else if ($request->auth_number) {
 
             $Authorization->auth_number = $request->auth_number;
             $Authorization->auth_status_id = 3;
+        } else if ($request->observation) {
+
+            $Authorization->observation = $request->observation;
+            $Authorization->auth_status_id = 4;
         } else {
+
             $Authorization->auth_status_id = $request->auth_status_id;
         }
 
         $Authorization->save();
-
+        
         $auth_log = new AuthLog;
 
         $auth_log->current_status_id = $Authorization->auth_status_id;
@@ -178,6 +212,7 @@ class AuthorizationController extends Controller
         $auth_log->user_id = Auth::user()->id;
 
         $auth_log->save();
+
 
 
         return response()->json([
