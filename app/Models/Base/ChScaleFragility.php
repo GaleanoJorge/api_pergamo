68b7b52e27aa85c6f40c7187a9604b9dac1204f3
Ana M Lopez <annam.lopezb@gmail.com>
2022-04-28 08:47:43 -0500

    Creación escala Fragility

---
+++ app/Models/Base/ChScaleFragility.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models\Base;
+
+use App\Models\ChTypeRecord;
+use App\Models\ChRecord;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Bed
+ * 
+ * @property int $id 
+  * @property int $question_one
+ * @property int $question_two
+ * @property int $question_three
+ * @property int $question_four
+ * @property int $question_five
+ * @property int $question_six
+ * @property int $total
+ * @property string $classification
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * 
+ *
+ * @package App\Models\Base
+ */
+class ChScaleFragility extends Model
+{
+	protected $table = 'ch_scale_fragility';
+
+	public function type_record()
+	{
+		return $this->belongsTo(ChTypeRecord::class);
+	}
+	public function ch_record()
+	{
+		return $this->belongsTo(ChRecord::class);
+	}
+}
