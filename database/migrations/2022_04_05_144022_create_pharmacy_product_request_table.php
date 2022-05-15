7e6641232777c830a531a8b1779525d8c3af17c4
Harold <gidesarrolladorapp2@hlips.com.co>
2022-05-15 12:11:47 -0500

    FARMACIA PRODUCTO REQUERIDO TODA LOGICA

--- database/migrations/2022_04_05_144022_create_pharmacy_product_request_table.php
+++ database/migrations/2022_04_05_144022_create_pharmacy_product_request_table.php
@@ -16,24 +16,31 @@ class CreatePharmacyProductRequestTable extends Migration
     {
         Schema::create('pharmacy_product_request', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('amount');
+            $table->integer('request_amount')->nullable();
+            $table->string('status');
+            $table->string('observation')->nullable();
             $table->unsignedBigInteger('product_generic_id');
-            $table->unsignedBigInteger('pharmacy_stock_id');
+            $table->unsignedBigInteger('own_pharmacy_stock_id');
+            $table->unsignedBigInteger('request_pharmacy_stock_id');
             $table->timestamps();
 
             $table->index('product_generic_id');
             $table->foreign('product_generic_id')->references('id')
                 ->on('product_generic');
 
-            $table->index('pharmacy_stock_id');
-            $table->foreign('pharmacy_stock_id')->references('id')
+            $table->index('own_pharmacy_stock_id');
+            $table->foreign('own_pharmacy_stock_id')->references('id')
+                ->on('pharmacy_stock');
+
+            $table->index('request_pharmacy_stock_id');
+            $table->foreign('request_pharmacy_stock_id')->references('id')
                 ->on('pharmacy_stock');
         });
     }
 
     /**
      * Reverse the migrations.
-     *
+     *|
      * @return void
      */
     public function down()
