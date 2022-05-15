f7e995974b96057c838aacc6a062dfb74bfa20f5
Harold <gidesarrolladorapp2@hlips.com.co>
2022-05-15 12:16:46 -0500

    FARMACIA USUARIOS QUITAR ADMISIION

--- app/Http/Controllers/Management/PharmacyStockController.php
+++ app/Http/Controllers/Management/PharmacyStockController.php
@@ -50,42 +50,6 @@ class PharmacyStockController extends Controller
         ]);
     }
 
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function byadmission(Request $request, int $id): JsonResponse
-    {
-        $PharmacyStock = PharmacyStock::where('admissions_id', $id);
-
-        if ($request->_sort) {
-            $PharmacyStock->orderBy($request->_sort, $request->_order);
-        }
-
-        if ($request->search) {
-            $PharmacyStock->where('status', 'like', '%' . $request->search . '%');
-        }
-
-        if ($request->query("pagination", true) == "false") {
-            $PharmacyStock = $PharmacyStock->get()->toArray();
-        } else {
-            $page = $request->query("current_page", 1);
-            $per_page = $request->query("per_page", 10);
-
-            $PharmacyStock = $PharmacyStock->paginate($per_page, '*', 'page', $page);
-        }
-
-
-        return response()->json([
-            'status' => true,
-            'message' => 'Typo de establecimiento obtenidos exitosamente',
-            'data' => ['pharmacy_stock' => $PharmacyStock]
-        ]);
-    }
-
-
     public function store(Request $request): JsonResponse
     {
         $PharmacyStock = new PharmacyStock;
