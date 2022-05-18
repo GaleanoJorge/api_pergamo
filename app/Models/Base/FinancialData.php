f4aff1e13bdd1454115c4bfb5142c8384cb6ec4b
Jorge Galeano <gidesarrolladorapp1@hlips.com.co>
2022-05-18 12:01:11 -0500

    corrección en campos de financialdata y eliminación de bank information

--- app/Models/Base/FinancialData.php
+++ app/Models/Base/FinancialData.php
@@ -6,6 +6,9 @@
 
 namespace App\Models\Base;
 
+use App\Models\AccountType;
+use App\Models\Bank;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -15,7 +18,8 @@ use Illuminate\Database\Eloquent\Model;
  * 
  * @property int $id
  * @property bigInteger $user_id
- * @property bigInteger $bank_information_id
+ * @property bigInteger $bank_id
+ * @property bigInteger $account_type_id
  * @property string $rut
  * @property Carbon $created_at
  * @property Carbon $updated_at
@@ -27,5 +31,16 @@ class FinancialData extends Model
 {
 	protected $table = 'financial_data';
 
-	
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+	public function bank()
+	{
+		return $this->belongsTo(Bank::class);
+	}
+	public function account_type()
+	{
+		return $this->belongsTo(AccountType::class);
+	}
 }
