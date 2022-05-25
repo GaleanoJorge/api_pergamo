7e47575c9169811bf1cf4240d456498dabfdf307
Ana M Lopez <annam.lopezb@gmail.com>
2022-05-25 13:15:50 -0500

    ajuste campos escala FAC

--- app/Http/Controllers/Management/ChScaleFacController.php
+++ app/Http/Controllers/Management/ChScaleFacController.php
@@ -5,59 +5,65 @@ namespace App\Http\Controllers\Management;
 use App\Models\ChScaleFac;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request; 
+use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 
 class ChScaleFacController extends Controller
 {
-       /**
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request): JsonResponse
     {
-        $ChScaleFac = ChScaleFac::select();
+        if ($request->latest) {
+            $ChScaleFac = ChScaleFac::where('ch_record_id', $request->ch_record_id)->orderBy('created_at', 'desc')->take(1);
+        } else {
 
-        if($request->_sort){
-            $ChScaleFac->orderBy($request->_sort, $request->_order);
-        }            
-
-        if ($request->search) {
-            $ChScaleFac->where('name','like','%' . $request->search. '%');
+            $ChScaleFac = ChScaleFac::select();
         }
-        
-        if($request->query("pagination", true)=="false"){
-            $ChScaleFac=$ChScaleFac->get()->toArray();    
+            if ($request->_sort) {
+                $ChScaleFac->orderBy($request->_sort, $request->_order);
+            }
+
+            if ($request->search) {
+                $ChScaleFac->where('name', 'like', '%' . $request->search . '%');
+            }
+            if ($request->ch_record_id) {
+                $ChScaleFac->where('ch_record_id', $request->ch_record_id);
+            }
+            if ($request->latest  && isset($request->latest)) {
+            }
+            if ($request->query("pagination", true) == "false") {
+                $ChScaleFac = $ChScaleFac->get()->toArray();
+            } else {
+                $page = $request->query("current_page", 1);
+                $per_page = $request->query("per_page", 10);
+
+                $ChScaleFac = $ChScaleFac->paginate($per_page, '*', 'page', $page);
+            }
+
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Escala FAC obtenida exitosamente',
+                'data' => ['ch_scale_fac' => $ChScaleFac]
+            ]);
         }
-        else{
-            $page= $request->query("current_page", 1);
-            $per_page=$request->query("per_page", 10);
-            
-            $ChScaleFac=$ChScaleFac->paginate($per_page,'*','page',$page); 
-        } 
-
-
-        return response()->json([
-            'status' => true,
-            'message' => 'Escala FAC obtenida exitosamente',
-            'data' => ['ch_scale_fac' => $ChScaleFac]
-        ]);
-    }
     
-    
-        /**
+    /**
      * Display the specified resource.
      *
      * @param  int  $id
      * @return JsonResponse
      */
-    public function getByRecord(int $id,int $type_record_id): JsonResponse
-    {       
-       
-        $ChScaleFac = ChScaleFac::where('ch_record_id', $id)->where('type_record_id',$type_record_id)
-        ->get()->toArray();
-        
+    public function getByRecord(int $id, int $type_record_id): JsonResponse
+    {
+
+        $ChScaleFac = ChScaleFac::where('ch_record_id', $id)->where('type_record_id', $type_record_id)
+            ->get()->toArray();
+
 
         return response()->json([
             'status' => true,
@@ -68,11 +74,12 @@ class ChScaleFacController extends Controller
 
     public function store(Request $request): JsonResponse
     {
-        $ChScaleFac = new ChScaleFac; 
-        $ChScaleFac->level = $request->level; 
-        $ChScaleFac->definition= $request->definition;
-        $ChScaleFac->type_record_id = $request->type_record_id; 
-        $ChScaleFac->ch_record_id = $request->ch_record_id; 
+        $ChScaleFac = new ChScaleFac;
+        $ChScaleFac->level_title = $request->level_title;
+        $ChScaleFac->level_value = $request->level_value;
+        $ChScaleFac->definition = $request->definition;
+        $ChScaleFac->type_record_id = $request->type_record_id;
+        $ChScaleFac->ch_record_id = $request->ch_record_id;
         $ChScaleFac->save();
 
         return response()->json([
@@ -109,11 +116,12 @@ class ChScaleFacController extends Controller
      */
     public function update(Request $request, int $id): JsonResponse
     {
-        $ChScaleFac = ChScaleFac::find($id);  
-        $ChScaleFac->level = $request->level; 
-        $ChScaleFac->definition= $request->definition;
-        $ChScaleFac->type_record_id = $request->type_record_id; 
-        $ChScaleFac->ch_record_id = $request->ch_record_id; 
+        $ChScaleFac = ChScaleFac::find($id);
+        $ChScaleFac->level_title = $request->level_title;
+        $ChScaleFac->level_value = $request->level_value;
+        $ChScaleFac->definition = $request->definition;
+        $ChScaleFac->type_record_id = $request->type_record_id;
+        $ChScaleFac->ch_record_id = $request->ch_record_id;
         $ChScaleFac->save();
 
         return response()->json([
@@ -133,7 +141,7 @@ class ChScaleFacController extends Controller
     {
         try {
             $ChScaleFac = ChScaleFac::find($id);
-            
+
             $ChScaleFac->delete();
 
             return response()->json([
