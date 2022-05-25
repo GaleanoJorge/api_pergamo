5313fc6a187f18680ebbf8ba2a35e31fe3b8d31a
Ana M Lopez <annam.lopezb@gmail.com>
2022-05-25 13:14:48 -0500

    ajuste campos escala ECOG

--- app/Models/ChScaleEcog.php
+++ app/Models/ChScaleEcog.php
@@ -8,8 +8,9 @@ use App\Models\Base\ChScaleEcog as BaseChScaleEcog;
 class ChScaleEcog extends BaseChScaleEcog
 {
   protected $fillable = [
-    'grade',
-   'definition',
+    'grade_title',
+    'grade_value',
+    'definition',
     'type_record_id',
     'ch_record_id',
   ];
