c4e2465699bcf3e5eab33dd8f12b4c7f5e833645
Adriana Parra <gianalista@hlips.com.co>
2022-06-15 15:08:58 -0500

    chtl Crud Sesiones

---
+++ app/Models/Base/NumberMonthlySessionsTl.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models\Base;
+
+use App\Models\ChRecord;
+use App\Models\ChTypeRecord;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class NumberMonthlySessionsTl
+ * 
+ * @property int $id
+ * @property string $monthly_sessions
+ * @property string $weekly_intensity 
+ * @property string $recomendations 
+ * @property BigInteger $type_record_id
+ * @property BigInteger $ch_record_id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * 
+ *
+ * @package App\Models\Base
+ */
+class NumberMonthlySessionsTl extends Model
+{
+	protected $table = 'number_monthly_sessions_tl';
+
+	public function type_record()
+	{
+		return $this->belongsTo(ChTypeRecord::class);
+	}
+	public function ch_record()
+	{
+		return $this->belongsTo(ChRecord::class);
+	}
+
+}
