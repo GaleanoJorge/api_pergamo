d8cadb3047d4023903f327d1504b8bf0f0ac9917
Ivan Sanchez <gidesarrollador@hlips.com.co>
2022-07-15 11:03:37 -0500

    mod: agregar filtro por usuario y status

--- app/Http/Controllers/Management/PharmacyRequestShippingController.php
+++ app/Http/Controllers/Management/PharmacyRequestShippingController.php
@@ -34,6 +34,15 @@ class PharmacyRequestShippingController extends Controller
             )
             ->leftJoin('pharmacy_product_request', 'pharmacy_product_request.id', 'pharmacy_request_shipping.pharmacy_product_request_id');
 
+        if ($request->user_id) {
+            $PharmacyRequestShipping->where('pharmacy_product_request.user_request_id', $request->user_id);
+        }
+
+        
+        if ($request->status) {
+            $PharmacyRequestShipping->where('pharmacy_product_request.status', $request->status);
+        }
+        
         if ($request->_sort) {
             $PharmacyRequestShipping->orderBy($request->_sort, $request->_order);
         }
