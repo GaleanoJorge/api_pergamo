4f131cc3701dbf493d83db4929f524982a3fb874
Harold Cardenas <gidesarrolladorapp2@hlips.com.co>
2022-07-19 15:57:56 -0500

    provicional activos

--- database/migrations/2022_05_20_094411_create_fixed_assets_table.php
+++ database/migrations/2022_05_20_094411_create_fixed_assets_table.php
@@ -16,35 +16,72 @@ class CreateFixedAssetsTable extends Migration
         Schema::create('fixed_assets', function (Blueprint $table) {
             $table->BigIncrements('id');
             $table->unsignedBigInteger('fixed_clasification_id');
-            $table->unsignedBigInteger('fixed_type_role_id');
+            $table->unsignedBigInteger('fixed_type_id');
             $table->unsignedBigInteger('fixed_property_id');
             $table->string('obs_property')->nullable();
             $table->string('plaque')->nullable();
             $table->unsignedBigInteger('company_id')->nullable();
-            $table->integer('amount');
+            $table->integer('amount_total');
             $table->integer('actual_amount');
             $table->string('model')->nullable();
             $table->string('mark');
             $table->string('serial')->nullable();
-            $table->string('description');
+            $table->unsignedBigInteger('fixed_nom_product_id');
             $table->string('detail_description');
             $table->string('color');
             $table->string('status');
             $table->unsignedBigInteger('fixed_condition_id');
             $table->unsignedBigInteger('campus_id');
+
+
+            $table->string('calibration_certificate')->nullable();
+            $table->string('health_register')->nullable();
+            $table->string('warranty')->nullable();
+            $table->string('cv')->nullable();
+            $table->date('last_maintenance')->nullable();
+            $table->date('last_pame')->nullable();
+            $table->string('interventions_carriet')->nullable();
+            $table->string('type')->nullable();
+            $table->string('mobile_fixed')->nullable();
+            $table->unsignedBigInteger('clasification_risk_id')->nullable();
+            $table->unsignedBigInteger('biomedical_classification_id')->nullable();
+            $table->string('code_ecri')->nullable();
+            $table->string('form_acquisition')->nullable();
+            $table->date('date_adquisicion')->nullable();
+            $table->date('date_warranty')->nullable();
+            $table->string('useful_life')->nullable();
+            $table->integer('cost')->nullable();
+            $table->string('maker')->nullable();
+            $table->string('phone_maker')->nullable();
+            $table->string('email_maker')->nullable();
+            $table->string('power_supply')->nullable();
+            $table->string('predominant_technology')->nullable();
+            $table->string('volt')->nullable();
+            $table->string('stream')->nullable();
+            $table->string('power')->nullable();
+            $table->string('frequency_rank')->nullable();
+            $table->string('temperature_rank')->nullable();
+            $table->string('humidity_rank')->nullable();
+            $table->string('manuals')->nullable();
+            $table->string('guide')->nullable();
+            $table->unsignedTinyInteger('periodicity_frequency_id')->nullable();
+            $table->unsignedTinyInteger('calibration_frequency_id')->nullable();
             $table->timestamps();
 
             $table->index('fixed_clasification_id');
             $table->foreign('fixed_clasification_id')->references('id')
                 ->on('fixed_clasification');
+            $table->index('fixed_nom_product_id');
+            $table->foreign('fixed_nom_product_id')->references('id')
+                ->on('fixed_nom_product');
 
             $table->index('company_id');
             $table->foreign('company_id')->references('id')
                 ->on('company');
 
-            $table->index('fixed_type_role_id');
-            $table->foreign('fixed_type_role_id')->references('id')
-                ->on('fixed_type_role');
+            $table->index('fixed_type_id');
+            $table->foreign('fixed_type_id')->references('id')
+                ->on('fixed_type');
 
             $table->index('fixed_property_id');
             $table->foreign('fixed_property_id')->references('id')
@@ -57,6 +94,21 @@ class CreateFixedAssetsTable extends Migration
             $table->index('campus_id');
             $table->foreign('campus_id')->references('id')
                 ->on('campus');
+
+            $table->index('clasification_risk_id');
+            $table->foreign('clasification_risk_id')->references('id')
+                ->on('risk');
+            $table->index('biomedical_classification_id');
+            $table->foreign('biomedical_classification_id')->references('id')
+                ->on('biomedical_classification');
+
+            $table->index('periodicity_frequency_id');
+            $table->foreign('periodicity_frequency_id')->references('id')
+                ->on('frequency');
+
+            $table->index('calibration_frequency_id');
+            $table->foreign('calibration_frequency_id')->references('id')
+                ->on('frequency');
         });
     }
 
