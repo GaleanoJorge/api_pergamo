af52137c7568a6e352c2cb13be7df40d4058bbee
Jorge Galeano <gidesarrolladorapp1@hlips.com.co>
2022-08-16 07:49:55 -0500

    corrección firma en pdf hc

--- resources/views/mails/hc.blade.php
+++ resources/views/mails/hc.blade.php
@@ -6137,6 +6137,41 @@ Detalles
 
     @endisset
     </div>
+
+
+
+     <!-- Firma -->
+    <div>
+        <br>
+        <br>
+        <hr />
+        @if($firm != null)
+        <p style="margin-top:15pt; margin-left:8pt; margin-bottom:0pt;">
+            <span style="height:0pt;">
+            
+                <img src="data:image/png;base64,{{$firm}}" width="250" height="100" alt="" style=""/></span>
+                <span style="width:171.33pt; display:inline-block; -aw-tabstop-align:left; -aw-tabstop-pos:257.05pt">&#xa0;</span>
+            </p>
+            @endisset
+            <p style="margin-top:8.95pt; margin-left:8pt; margin-bottom:0pt; widows:0; orphans:0; font-size:9pt">
+                <span style="font-family:Calibri; font-weight:bold; color:#000000; background-color:#ffffff">{{$chrecord[0]['user']['firstname']}} {{$chrecord[0]['user']['middlefirstname']}} {{$chrecord[0]['user']['lastname']}}  {{$chrecord[0]['user']['middlelastname']}}</span>
+                <span style="width:171.33pt; display:inline-block; -aw-tabstop-align:left; -aw-tabstop-pos:257.05pt">&#xa0;</span>
+            </p>
+            <p style="margin-top:8.95pt; margin-left:8pt; margin-bottom:0pt; widows:0; orphans:0; font-size:9pt">
+                <span style="font-family:Calibri; font-weight:bold; color:#000000; background-color:#ffffff">{{$chrecord[0]['user']['user_role'][0]['role']['name']}}</span>
+                <span style="width:171.33pt; display:inline-block; -aw-tabstop-align:left; -aw-tabstop-pos:257.05pt">&#xa0;</span>
+            </p>
+            @if(count($chrecord[0]['user']['assistance']) > 0)
+            <p style="margin-top:8.95pt; margin-left:8pt; margin-bottom:0pt; widows:0; orphans:0; font-size:9pt">
+                <span style="font-family:Calibri; font-weight:bold; color:#000000; background-color:#ffffff">RM/TP: {{$chrecord[0]['user']['assistance'][0]['medical_record']}}</span>
+                <span style="width:171.33pt; display:inline-block; -aw-tabstop-align:left; -aw-tabstop-pos:257.05pt">&#xa0;</span>
+            </p>
+            
+            
+    
+            @endisset   
+    </div>
+
 </body>
 
 </html>
