3e6a84a4a66c57ce8951f55a1e216f4d7a7a8160
Daniel Montañez <gibasedatosydesarrollo@hlips.com.co>
2022-09-26 18:02:26 -0500

    Ajuste terapias hc

--- app/Http/Controllers/Management/SwallowingDisordersController.php
+++ app/Http/Controllers/Management/SwallowingDisordersController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 use App\Models\ChRecord;
 
-class SwallowingDisordersTLController extends Controller
+class SwallowingDisordersController extends Controller
 {
     /**
      * Display a listing of the resource.
--- routes/api-management.php
+++ routes/api-management.php
@@ -1580,8 +1580,8 @@ Route::group(['middleware' => ['cors', 'jwt.auth', 'api']], function () {
     Route::apiResource('speech_tl', 'Management\SpeechTlController');
     Route::get('speech_tl/by_record/{id}/{type_record_id}', 'Management\SpeechTlController@getByRecord');
 
-    Route::apiResource('swallowing_disorders_tl', 'Management\SwallowingDisordersTLController');
-    Route::get('swallowing_disorders_tl/by_record/{id}/{type_record_id}', 'Management\SwallowingDisordersTLController@getByRecord');
+    Route::apiResource('swallowing_disorders_tl', 'Management\SwallowingDisordersController');
+    Route::get('swallowing_disorders_tl/by_record/{id}/{type_record_id}', 'Management\SwallowingDisordersController@getByRecord');
 
     Route::apiResource('therapeutic_goals_tl', 'Management\TherapeuticGoalsTlController');
     Route::get('therapeutic_goals_tl/by_record/{id}/{type_record_id}', 'Management\TherapeuticGoalsTlController@getByRecord');
