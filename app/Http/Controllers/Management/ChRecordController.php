2945082c0944aa72a48871b914c3c3c3c9db4e90
Ana María <gi.desarrollador4@hlips.com.co>
2022-09-30 12:42:36 -0500

    Se crea PDF OrdenMedica

--- app/Http/Controllers/Management/ChRecordController.php
+++ app/Http/Controllers/Management/ChRecordController.php
@@ -491,6 +491,106 @@ class ChRecordController extends Controller
             ]);
     }
 
+    public function ViewMedicalOrder(int $id)
+    {
+        $fecharecord = 0;
+
+
+
+        //Ordenes Médicas
+        $ChMedicalOrders = ChMedicalOrders::with(
+            'procedure',
+            'frequency'
+        )
+            ->where('id', $id)->get()->toArray();
+
+
+        $ChRecord = ChRecord::with(
+            'user',
+            'user.assistance',
+            'user.user_role.role',
+            'admissions.contract',
+            'admissions.contract.company',
+            'admissions',
+            'admissions.patients',
+            'admissions.patients.academic_level',
+            'admissions.patients.municipality',
+            'admissions.patients.ethnicity',
+            'admissions.patients.gender',
+            'admissions.patients.identification_type',
+            'admissions.patients.residence_municipality',
+            'admissions.patients.residence',
+            'admissions.patients.marital_status',
+            'admissions.patients.population_group',
+            'admissions.patients.activities',
+            'admissions.contract.type_briefcase',
+            'assigned_management_plan',
+            'assigned_management_plan.management_plan',
+            'assigned_management_plan.management_plan.type_of_attention',
+            'assigned_management_plan.management_plan.procedure.manual_price',
+            'assigned_management_plan.management_plan.service_briefcase.manual_price',
+            'assigned_management_plan.management_plan.route_administration',
+            // 'assistance_supplies',
+            // 'assistance_supplies.user_incharge_id',
+            // 'assistance_supplies.application_hour',
+        )
+
+
+            ->get()->toArray();
+
+            $imagenComoBase64 = null;
+
+            $fecharecord = Carbon::parse($ChRecord[0]['updated_at'])->format('d-m-Y h:i:s');
+            
+
+
+            if (isset($ChRecord[0]['user']['assistance'][0]['file_firm']) && $ChRecord[0]['user']['assistance'][0]['file_firm'] != "null") {
+                $rutaImagen = storage_path('app/public/' . $ChRecord[0]['user']['assistance'][0]['file_firm']);
+                $contenidoBinario = file_get_contents($rutaImagen);
+                $imagenComoBase64 = base64_encode($contenidoBinario);
+            } else {
+                $imagenComoBase64 = null;
+            }
+    
+            $today = Carbon::now();
+    
+            $Patients = $ChRecord[0]['admissions']['patients'];
+    
+    
+            $html = view('mails.chMedicalOrder', [
+                'chrecord' => $ChRecord,
+                'ChMedicalOrders' => $ChMedicalOrders,
+                'fecharecord' => $fecharecord,
+                'firm' => $imagenComoBase64,
+                'today' => $today,
+    
+            ])->render();
+    
+            $options = new Options();
+            $options->set('isRemoteEnabled', true);
+            $dompdf = new PDF($options);
+            $dompdf->loadHtml($html);
+            $dompdf->setPaper('Carta', 'portrait');
+            $dompdf->render();
+            $this->injectPageCount($dompdf);
+            $file = $dompdf->output();
+    
+            $name = 'ordenmedica.pdf';
+    
+            Storage::disk('public')->put($name, $file);
+    
+    
+    
+    
+            return response()->json([
+                'status' => true,
+                'persona' => $ChMedicalOrders,
+                'ch' => $ChRecord,
+                'message' => 'Reporte generado exitosamente',
+                'url' => asset('/storage' . '/' . $name),
+            ]);
+    }
+
 
     public function ViewHC(int $id)
     {
