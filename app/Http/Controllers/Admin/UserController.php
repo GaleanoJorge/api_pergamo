f101b57b2a1ed789ea2868f40dfae239f0a0e8bc
Jorge Galeano <gi.basedatosydesarrollo@hlips.com.co>
2022-10-01 15:04:07 -0500

    ajuste personal

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -337,11 +337,16 @@ class UserController extends Controller
                 //         $query->orWhere('user_role.role_id', $role->role_id);
                 //     }
                 // }
-                if ($first) {
-                    $query->where('user_role.role_id', $role->role_id);
-                    $first = false;
+                if ($role->role_id == 14/* || $role->role_id == 7*/) {
+                    $specialty = RoleAttention::select()->where('role_id', $role->role_id)->where('type_of_attention_id',  $request->type_of_attention)->get()->first();
+                    $query->where('assistance_special.specialty_id', $specialty->specialty_id);
                 } else {
-                    $query->orWhere('user_role.role_id', $role->role_id);
+                    if ($first) {
+                        $query->where('user_role.role_id', $role->role_id);
+                        $first = false;
+                    } else {
+                        $query->orWhere('user_role.role_id', $role->role_id);
+                    }
                 }
             }
         });
