e700b1e9fbc7008255b4f935a8a6b440f8bd0cd3
Iván La roca Sánchez <gi.desarrollador3@hlips.com>
2022-10-07 09:36:28 -0500

    mod: relaciones cita medica

--- app/Models/Base/MedicalDiaryDays.php
+++ app/Models/Base/MedicalDiaryDays.php
@@ -6,10 +6,16 @@
 
 namespace App\Models\Base;
 
+use App\Models\Contract;
 use App\Models\Days;
+use App\Models\MedicalStatus;
+use App\Models\Patient;
+use App\Models\ServicesBriefcase;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
 
 /**
  * Class Bank
@@ -31,5 +37,34 @@ class MedicalDiaryDays extends Model
 	public function days()
 	{
 		return $this->belongsTo(Days::class);
+	}
+
+	public function medical_status()
+	{
+		return $this->belongsTo(MedicalStatus::class, 'medical_status_id');
+	}
+
+	public function patient()
+	{
+		return $this->belongsTo(Patient::class, 'patient_id')->select(
+			'patients.*',
+			DB::raw('CONCAT_WS(" ",patients.lastname,patients.middlelastname,patients.firstname,patients.middlefirstname) AS nombre_completo')
+
+		);
+	}
+
+	public function contract()
+	{
+		return $this->belongsTo(Contract::class, 'contract_id');
+	}
+
+	public function briefcase()
+	{
+		return $this->belongsTo(Briefcase::class, 'briefcase_id');
+	}
+
+	public function services_briefcase()
+	{
+		return $this->belongsTo(ServicesBriefcase::class, 'services_briefcase_id');
 	}
 }
