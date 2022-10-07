d9800094d2a397e558bcd09a58f1ee45cc26b63d
Iván La roca Sánchez <gi.desarrollador3@hlips.com>
2022-10-07 09:40:22 -0500

    mod: agregar referenciación oficina para agenda

--- database/migrations/2022_07_23_110345_create_medical_diary_table.php
+++ database/migrations/2022_07_23_110345_create_medical_diary_table.php
@@ -16,28 +16,45 @@ class CreateMedicalDiaryTable extends Migration
         Schema::create('medical_diary', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('assistance_id');
-            $table->unsignedBigInteger('office_id')->nullable();
+            $table->unsignedBigInteger('campus_id');
+            $table->unsignedBigInteger('flat_id');
+            $table->unsignedBigInteger('pavilion_id');
+            $table->unsignedBigInteger('office_id');
             $table->unsignedTinyInteger('diary_status_id');
+            $table->unsignedBigInteger('procedure_id');
             $table->time('start_time');
             $table->time('finish_time');
             $table->date('start_date');
             $table->date('finish_date');
             $table->integer('interval');
             $table->boolean('attends_teleconsultation')->nullable();
+            $table->unsignedBigInteger('patient_quantity')->nullable();
             $table->timestamps();
 
 
             $table->index('assistance_id');
+            $table->index('campus_id');
+            $table->index('flat_id');
+            $table->index('pavilion_id');
             $table->index('office_id');
             $table->index('diary_status_id');
+            $table->index('procedure_id');
 
 
             $table->foreign('assistance_id')->references('id')
                 ->on('assistance');
+            $table->foreign('campus_id')->references('id')
+                ->on('campus');
+            $table->foreign('flat_id')->references('id')
+                ->on('flat');
+            $table->foreign('pavilion_id')->references('id')
+                ->on('pavilion');
             $table->foreign('office_id')->references('id')
                 ->on('bed');
             $table->foreign('diary_status_id')->references('id')
                 ->on('status');
+            $table->foreign('procedure_id')->references('id')
+                ->on('procedure');
         });
     }
 
