2c234de38630c4b1b197c9ec18c65afd40bf6854
Harold Cardenas <gidesarrolladorapp2@hlips.com.co>
2022-10-10 16:23:12 -0500

    supplies arreglos

--- app/Models/Base/ProductSupplies.php
+++ app/Models/Base/ProductSupplies.php
@@ -6,7 +6,10 @@
 
 namespace App\Models\Base;
 
+use App\Models\ProductCategory;
 use App\Models\ProductDose;
+use App\Models\ProductGroup;
+use App\Models\ProductSubcategory;
 use App\Models\SuppliesMeasure;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
@@ -26,6 +29,9 @@ use Illuminate\Database\Eloquent\Model;
  * @property BigInteger $product_dose_id
  * @property BigInteger $size_supplies_measure_id
  * @property BigInteger $measure_supplies_measure_id
+ * @property BigInteger $product_group_id
+ * @property BigInteger $product_category_id
+ * @property BigInteger $product_subcategory_id
  * @property Carbon $created_at
  * @property Carbon $updated_at
  * 
@@ -35,7 +41,7 @@ use Illuminate\Database\Eloquent\Model;
 class ProductSupplies extends Model
 {
 	protected $table = 'product_supplies';
- 
+
 	public function size_supplies_measure()
 	{
 		return $this->belongsTo(SuppliesMeasure::class);
@@ -49,5 +55,20 @@ class ProductSupplies extends Model
 	public function product_dose()
 	{
 		return $this->belongsTo(ProductDose::class);
+	}
+
+	public function product_group()
+	{
+		return $this->belongsTo(ProductGroup::class);
+	}
+
+	public function product_category()
+	{
+		return $this->belongsTo(ProductCategory::class);
+	}
+
+	public function product_subcategory()
+	{
+		return $this->belongsTo(ProductSubcategory::class);
 	}
 }
