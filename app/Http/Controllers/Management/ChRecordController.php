e1603d3232ea45b8f40683d62fc9c67afb1be78e
Jorge Galeano <gi.basedatosydesarrollo@hlips.com.co>
2022-10-25 15:13:22 -0500

    ajuste de firma

--- app/Http/Controllers/Management/ChRecordController.php
+++ app/Http/Controllers/Management/ChRecordController.php
@@ -4113,8 +4113,8 @@ class ChRecordController extends Controller
     public function store(Request $request): JsonResponse
     {
         $count = 0;
-        $chrecord = ChRecord::where('admissions_id', $request->admissions_id)->get()->toArray();
-        foreach ($chrecord as $ch) {
+        $chrecord_val = ChRecord::where('admissions_id', $request->admissions_id)->get()->toArray();
+        foreach ($chrecord_val as $ch) {
             $count++;
         }
         $ChRecord = new ChRecord;
@@ -4247,6 +4247,10 @@ class ChRecordController extends Controller
             Storage::disk('public')->put($imagePath, base64_decode($image));
 
             $ChRecord->file_firm = $imagePath;
+        } else {
+            if (count($chrecord_val) > 0) {
+                $ChRecord->file_firm = $chrecord_val[count($chrecord_val) - 1]['file_firm'];
+            }
         }
 
         $ChRecord->save();
