b7f199e039ca2406a607608613e4927c3ef1f6cf
Iván La roca Sánchez <gi.desarrollador3@hlips.com>
2022-11-01 10:53:13 -0500

    mod: relación con nombre completo

--- app/Models/Base/Assistance.php
+++ app/Models/Base/Assistance.php
@@ -11,6 +11,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\AssistanceSpecial;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
 /**
@@ -43,7 +44,10 @@ class Assistance extends Model
 
 	public function user()
 	{
-		return $this->belongsTo(User::class);
+		return $this->belongsTo(User::class)->select(
+			'users.*',
+			DB::raw('CONCAT_WS(" ",users.lastname,users.middlelastname,users.firstname,users.middlefirstname) AS nombre_completo')
+		);
 	}
 
 	public function assistance_procedure()
