ddd9ee60e07986d16d8a326fa6a21fda6c8c0248
Daniel Montañez <gibasedatosydesarrollo@hlips.com.co>
2022-11-10 07:22:34 -0500

    ajuste terapia respiratoria

--- resources/views/mails/respiratoryhistory.blade.php
+++ resources/views/mails/respiratoryhistory.blade.php
@@ -1686,7 +1686,7 @@
 
                     @if(isset($ch['product_supplies']))
                         <td>
-                            <span style="font-family:Calibri; font-size:9pt">{{$ch['product_supplies']['name'] ? $ch['product_supplies']['name'] : 'No registra'}} </span>
+                            <span style="font-family:Calibri; font-size:9pt">{{$ch['product_supplies']['description'] ? $ch['product_supplies']['description'] : 'No registra'}} </span>
                         </td>
                         @endisset
 
