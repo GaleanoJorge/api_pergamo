34212d29ffefec4093cc326061e11c6bd0163ec5
Ana María <gi.desarrollador4@hlips.com.co>
2022-11-15 15:13:36 -0500

    ajuste consulta

--- app/Http/Controllers/Management/ChRecordController.php
+++ app/Http/Controllers/Management/ChRecordController.php
@@ -1520,7 +1520,7 @@ class ChRecordController extends Controller
                 ->leftJoin('pharmacy_product_request', 'assistance_supplies.pharmacy_product_request_id', 'pharmacy_product_request.id')
                 ->leftJoin('services_briefcase', 'pharmacy_product_request.services_briefcase_id', 'services_briefcase.id')
                 ->leftJoin('manual_price', 'services_briefcase.manual_price_id', 'manual_price.id')
-                ->where('ch_record_id', $id)->where('manual_price.product_id','!=', null)
+                ->where('ch_record_id', $id)->whereNotNull('manual_price.product_id')
                 ->get()->toArray();
 
             if (isset($ChRecord[0]['user']['assistance'][0]['file_firm']) && $ChRecord[0]['user']['assistance'][0]['file_firm'] != "null") {
