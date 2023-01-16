f38d332f77eeb626566b3984193aa2efc635d72c
Jorge Galeano <gi.basedatosydesarrollo@hlips.com.co>
2023-01-16 18:49:50 -0500

    ajuste validación de tarifa en ch_record

--- app/Http/Controllers/Management/ChRecordController.php
+++ app/Http/Controllers/Management/ChRecordController.php
@@ -5268,7 +5268,7 @@ class ChRecordController extends Controller
 
             $valuetariff = $this->getNotFailedTariff($tariff, $ManagementPlan, $Location, $request, $admissions_id, $AssignedManagementPlan);
             if ($Assistance[0]['contract_type_id'] != 1 && $Assistance[0]['contract_type_id'] != 2 && $Assistance[0]['contract_type_id'] != 3) {
-                if (count($valuetariff) == 0) {
+                if (count($valuetariff) == 0 && !$ChRecord->ch_interconsultation_id) {
                     $extra_dose = 0;
                     $has_car = 0;
                     $Assistance = Assistance::select('assistance.*')
